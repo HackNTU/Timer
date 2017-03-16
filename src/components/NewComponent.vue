ff81a21c5bd030dfc04d651d5b516f10723a976e
choznerol <b02611002@ntu.edu.tw>
2017-03-16 23:15:53 +0800

    Add template: NewComponent.vue
    Get started by creating your first component like src/components/NewComponent.vue and register it App.vue !

---
+++ src/components/NewComponent.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+<div class="green-box">
+  <h1>{{msg}}</h1>
+  <i>Find me in src/components/NewComponent.vue</i>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'newcomponent',
+  data: function(){
+    return{
+      msg: 'hello <newcomponent>'
+    }
+  }
+
+}
+</script>
+
+<style lang="css">
+.green-box{
+  border: green 1px solid;
+}
+</style>

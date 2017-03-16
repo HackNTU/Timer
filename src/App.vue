20ffc115449db3698d71508dbaa9f7d19bdfae7f
choznerol <b02611002@ntu.edu.tw>
2017-03-16 23:29:44 +0800

    Add countdown component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
     <!-- Get started by creating your first component: https://github.com/HackNTU/Timer/commit/b0dc3586375a9a6a1ef7fdaf11628a391c4e6162 -->
-    <newcomponent></newcomponent>
+    <!-- <newcomponent></newcomponent> -->
+    <countdown deadline="Sun Mar 19 2017 14:00:00 GMT+0800 (CST)" id="countdown"></countdown>
   </div>
 </template>
 
 <script>
 import NewComponent from './components/NewComponent.vue'
+import Countdown from './components/Countdown.vue'
 export default {
   name: 'app',
   data () {
@@ -16,6 +18,7 @@ export default {
   },
   components: {
     newcomponent: NewComponent,
+    countdown: Countdown,
   }
 }
 </script>
@@ -46,5 +49,17 @@ li {
 
 a {
   color: #42b983;
+}
+
+#countdown {
+  align-items: center;
+  bottom: 0;
+  /*background-color: #34495e;*/
+  display: flex;
+  justify-content: center;
+  left: 0;
+  position: absolute;
+  right: 0;
+  top:0;
 }
 </style>
